0aa9c3fb4dfef8632eabe820cfa7e85d4d6ce336
Maxington20 <max.herrington@gmail.com>
2023-04-22 22:21:52 -0400

    added new pages, updated some styles

--- WhatsForDinner/AppShell.xaml.cs
+++ WhatsForDinner/AppShell.xaml.cs
@@ -5,5 +5,8 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute(nameof(GenerateFromIngredientsPage), typeof(GenerateFromIngredientsPage));
+        //Routing.RegisterRoute(nameof(MenuItem2Page), typeof(MenuItem2Page));
+        //Routing.RegisterRoute(nameof(MenuItem3Page), typeof(MenuItem3Page));
+    }
 }
