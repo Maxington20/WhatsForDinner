09e4831bc5c5a0b5b752921e05b7d6a4254633f4
Maxington20 <max.herrington@gmail.com>
2023-04-22 23:39:26 -0400

    more changes

--- WhatsForDinner/GenerateFromIngredientsPage.xaml.cs
+++ WhatsForDinner/GenerateFromIngredientsPage.xaml.cs
@@ -40,11 +40,25 @@ public partial class GenerateFromIngredientsPage : ContentPage
     {
         // Implement your recipe generation logic or API call here
         // For this example, we return an empty list
-        return new List<Recipe> { new Recipe { Name = "test recipe" } };
+        return new List<Recipe> { new Recipe { Name = "test recipe" }, new Recipe { Name = "pasta with cheese sauce" } };
     }
 
     public class Recipe
     {
         public string Name { get; set; }
+    }
+
+    private void OnRemoveRecipeClicked(object sender, EventArgs e)
+    {
+        if (sender is Button removeButton)
+        {
+            var recipeToRemove = removeButton.CommandParameter as Recipe;
+            if (recipeToRemove != null)
+            {
+                // Remove the recipe from the collection.
+                // Assuming you have an ObservableCollection<Recipe> named Recipes
+                _recipes.Remove(recipeToRemove);
+            }
+        }
     }
 }
