1a0b323492cda629769cef668dd66fc8efd42171
Maxington20 <max.herrington@gmail.com>
2023-04-25 16:21:29 -0400

    basic calendar working

--- WhatsForDinner/Pages/MealCalendarPage.xaml.cs
+++ WhatsForDinner/Pages/MealCalendarPage.xaml.cs
@@ -1,9 +1,23 @@
 namespace WhatsForDinner.Pages;
+using WhatsForDinner.Controls;
 
 public partial class MealCalendarPage : ContentPage
 {
 	public MealCalendarPage()
 	{
 		InitializeComponent();
-	}
+
+        var calendar = new CustomCalendar();
+        calendar.DaySelected += Calendar_DaySelected;
+
+        Content = new StackLayout
+        {
+            Children = { calendar }
+        };
+    }
+
+    private async void Calendar_DaySelected(object sender, DateTime e)
+    {
+        //await Navigation.PushAsync(new DayDetailPage(e));
+    }
 }
