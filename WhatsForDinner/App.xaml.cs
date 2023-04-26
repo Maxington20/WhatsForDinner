42ffbb7ed9e2cbf4d4759f479219a83d6a0e9787
Maxington20 <max.herrington@gmail.com>
2023-04-26 10:24:16 -0400

    now using syncfusion intead of custom calendar

--- WhatsForDinner/App.xaml.cs
+++ WhatsForDinner/App.xaml.cs
@@ -4,6 +4,7 @@ public partial class App : Application
 {
 	public App()
 	{
+		Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTg0Nzk1OUAzMjMxMmUzMTJlMzQzMUdINHNLck95clBQd0sydk9QaTA2N1M2UUNnMmxMbWdXdm1VbG04MFhBa1E9");
 		InitializeComponent();
 
 		MainPage = new AppShell();
--- WhatsForDinner/Pages/MealCalendarPage.xaml.cs
+++ WhatsForDinner/Pages/MealCalendarPage.xaml.cs
@@ -1,5 +1,7 @@
 namespace WhatsForDinner.Pages;
 using WhatsForDinner.Controls;
+using Syncfusion.Maui.Calendar;
+using Microsoft.Maui.Graphics.Text;
 
 public partial class MealCalendarPage : ContentPage
 {
@@ -7,13 +9,25 @@ public partial class MealCalendarPage : ContentPage
 	{
 		InitializeComponent();
 
-        var calendar = new CustomCalendar();
-        calendar.DaySelected += Calendar_DaySelected;
+      
 
-        Content = new StackLayout
+        SfCalendar calendar = new SfCalendar();        
+        this.Content = calendar;
+        this.Calendar = calendar;
+        this.Calendar.ShowActionButtons = true;
+        this.Calendar.TodayHighlightBrush = Color.FromRgba("#FFA500");
+        this.Calendar.SelectionBackground = Color.FromRgba("#FFA500");
+
+        CalendarTextStyle textStyle = new CalendarTextStyle()
         {
-            Children = { calendar }
-        };
+            TextColor = Color.FromRgba("#FFA500")
+    };
+
+        this.Calendar.MonthView = new CalendarMonthView()
+        {
+            //TodayBackground = Colors.Orange,
+            TodayTextStyle = textStyle
+        };                 
     }
 
     private async void Calendar_DaySelected(object sender, DateTime e)
